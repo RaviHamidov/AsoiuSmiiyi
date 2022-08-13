6118e25d5685f36e532969527aa54cbfe648ad9c
ravihamidov <ravihamidov42@gmail.com>
2022-08-13 13:43:39 +0400

    Fix: 1.Main page's 'Elana get' button is fixed 2. 'Elanlar' section's design changed a bit 3. 'Rehberlik' and 'Muellimler' sections design are changed 4. Add hover effect i mean : 'Bizim ixtisaslar' section

--- application/views/user/cabinet/passrenew.php
+++ application/views/user/cabinet/passrenew.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <link rel="icon" href="<?php echo base_url('assets/user/images');?>/logo.png">
     <!--Bootstrap-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
@@ -24,24 +24,26 @@
     <script src="<?php echo base_url('assets/user/js/') ?>cabinet.js"></script>
     <title>İstifadəçi paneli</title>
 </head>
+
 <body>
 
-    
 
-<div class = "h-100 d-flex align-items-center justify-content-center">
-    <div class="main-card card m-5 " style = "width:30%;">
-<div class = "py-4 px-4 my-1 mx-1">
 
-  <form action="<?php echo base_url('renewpass_act'); ?>" method="post" enctype="multipart/form-data">
-                    <input type="hidden" name="<?= $this->security->get_csrf_token_name(); ?>" value="<?= $this->security->get_csrf_hash();?>" />
+    <div class="h-100 d-flex align-items-center justify-content-center">
+        <div class="main-card card m-5 " style="width:30%;">
+            <div class="py-4 px-4 my-1 mx-1">
 
-                   
+                <form action="<?php echo base_url('renewpass_act'); ?>" method="post" enctype="multipart/form-data">
+                    <input type="hidden" name="<?= $this->security->get_csrf_token_name(); ?>"
+                        value="<?= $this->security->get_csrf_hash();?>" />
+
+
                     <div id="aze" class="tab-content current">
                         <label for="current">Cari şifrənizi daxil edin</label>
                         <input type="password" name="current" id="taze" class="form-control" placeholder="cari şifrə">
                         <!-- form validation start display show -->
                         <?php if(isset($form_error)){ ?>
-                            <small class="form_error_style"><?php echo form_error("cari"); ?></small>
+                        <small class="form_error_style"><?php echo form_error("cari"); ?></small>
                         <?php } ?>
                         <!-- form validation end display show -->
                         <br>
@@ -50,39 +52,48 @@
                         <input type="password" name="new1" id="taze" class="form-control" placeholder="yeni şifrə">
                         <!-- form validation start display show -->
                         <?php if(isset($form_error)){ ?>
-                            <small class="form_error_style"><?php echo form_error("new1"); ?></small>
+                        <small class="form_error_style"><?php echo form_error("new1"); ?></small>
                         <?php } ?>
                         <!-- form validation end display show -->
                         <br>
 
                         <label for="new2">Yeni şifrənizi təkrar daxil edin</label>
-                        <input type="password" name="new2" id="taze" class="form-control" placeholder="yeni şifrə təkrar">
+                        <input type="password" name="new2" id="taze" class="form-control"
+                            placeholder="yeni şifrə təkrar">
                         <!-- form validation start display show -->
                         <?php if(isset($form_error)){ ?>
-                            <small class="form_error_style"><?php echo form_error("new2"); ?></small>
+                        <small class="form_error_style"><?php echo form_error("new2"); ?></small>
                         <?php } ?>
                         <!-- form validation end display show -->
                         <br>
-                        
+
                     </div>
                     <?php if($this->session->flashdata('success')){ ?>
-    <b><p style="color:green; text-align:center;"><?php echo $this->session->flashdata('success') ?></p></b>
-  <?php } ?>
+                    <b>
+                        <p style="color:green; text-align:center;"><?php echo $this->session->flashdata('success') ?>
+                        </p>
+                    </b>
+                    <?php } ?>
 
 
-  <?php if($this->session->flashdata('err')){ ?>
-    <b><p style="color:red; text-align:center;"><?php echo $this->session->flashdata('err') ?></p></b>
-  <?php } ?>
-                    
-                        <div class="row">
+                    <?php if($this->session->flashdata('err')){ ?>
+                    <b>
+                        <p style="color:red; text-align:center;"><?php echo $this->session->flashdata('err') ?></p>
+                    </b>
+                    <?php } ?>
 
-                            <button type="submit" class="btn btn-info btn-block" style="margin-top: 23px; height: 40px;">Təsdiqlə</button>
-                        </div>
-                    
-                        </div>
-         
-                </form>
-                        </div></div>
-               
-                        </body>
-                        </html>
+                    <div class="row">
+
+                        <button type="submit" class="btn btn-info btn-block"
+                            style="margin-top: 23px; height: 40px;">Təsdiqlə</button>
+                    </div>
+
+            </div>
+
+            </form>
+        </div>
+    </div>
+
+</body>
+
+</html>
